45a8a070dc35e02729315e0bff9e5f6b12c429f9
Rijo Sebastian <rijobdk@gmail.com>
2024-06-09 10:55:56 +0530

    Add the delete route controller method and view  for the author, book, bookinstance, and genre models. - Add delete route for get and post methods for each model. - Add delete view for each model.

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -94,12 +94,49 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete GET");
+  const bookInstance = await BookInstance.findById(req.params.id)
+    .populate("book")
+    .exec();
+
+  if (bookInstance === null) {
+    // No results.
+    res.redirect("/catalog/bookinstances");
+  }
+
+  res.render("bookinstance_delete", {
+    title: "Delete BookInstance",
+    bookinstance: bookInstance,
+  });
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance delete POST");
+  // Assume valid BookInstance id in field.
+  await BookInstance.findByIdAndDelete(req.body.id);
+  res.redirect("/catalog/bookinstances");
+});
+
+// Display BookInstance update form on GET.
+exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
+  // Get book, all books for form (in parallel)
+  const [bookInstance, allBooks] = await Promise.all([
+    BookInstance.findById(req.params.id).populate("book").exec(),
+    Book.find(),
+  ]);
+
+  if (bookInstance === null) {
+    // No results.
+    const err = new Error("Book copy not found");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("bookinstance_form", {
+    title: "Update BookInstance",
+    book_list: allBooks,
+    selected_book: bookInstance.book._id,
+    bookinstance: bookInstance,
+  });
 });
 
 // Display BookInstance update form on GET.
